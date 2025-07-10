5d978d5292f59e03f056933342576433dc70e3fc
jbarneyy <js_barnett@outlook.com>
2025-07-10 12:28:38 -0700

    Test commit to add git credential key

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from textnode import TextType
 
 def main():
     dummy_node = TextNode("This is anchor text", TextType.LINK, "https://www.boot.dev")
+    second_node = TextNode("Any text", TextType.TEXT, "https://www.boot.dev")
     print(dummy_node)
 
 
