edb37dbddb13718a6c47cedaf0c682aa9d0677ef
jbarneyy <js_barnett@outlook.com>
2025-07-15 14:34:34 -0700

    Created copy_src_to_dst() func in main, copys all files in static/ to fresh public/ folder

--- src/main.py
+++ src/main.py
@@ -3,13 +3,45 @@ from htmlnode import HTMLNode
 from leafnode import LeafNode
 from parentnode import ParentNode
 
-from enum import Enum
-import re
+import os
+import shutil
 
 
 def main():
-    dummy_node = TextNode("This is anchor text", TextType.LINK, "https://www.boot.dev")
-    second_node = TextNode("Any text", TextType.TEXT, "https://www.boot.dev")
+    copy_src_to_dst("static", "public")
+
+
+def copy_src_to_dst(src: str, dst: str):
+    abs_src = os.path.abspath(src)
+    abs_dst = None
+
+    if os.path.exists(os.path.abspath(dst)):
+        abs_dst = os.path.abspath(dst)
+        shutil.rmtree(abs_dst)
+        os.mkdir(abs_dst)
+    else:
+        os.mkdir(os.path.abspath(dst))
+        abs_dst = os.path.abspath(dst)
+
+    src_to_dst_helper(abs_src, abs_dst)
+
+
+def src_to_dst_helper(src: str, dst: str):
+    src_list = os.listdir(src)
+
+    for item in src_list:
+        item_src_path = os.path.join(src, item)
+        item_dst_path = os.path.join(dst, item)
+
+        if os.path.isfile(item_src_path):
+            shutil.copy(item_src_path, item_dst_path)
+
+            print(f"Copying file: {item_src_path} ---> {item_dst_path}")
+        else:
+            os.mkdir(item_dst_path)
+
+            print(f"Creating new dir and recursing: {item_dst_path}")
+            src_to_dst_helper(item_src_path, item_dst_path)
 
 
 
